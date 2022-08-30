93bdf250404b5ec1e8c7048fa29468292956e35f
Leonard <leonard@mimisiku.dev>
2022-08-31 01:25:14 +0200

    cleanup code style

--- src/index.ts
+++ src/index.ts
@@ -6,71 +6,79 @@ import { decodeMigrationUrl } from './decode/decodeUrl';
 import { getOtpAuthUris } from './decode/getOtpURIs';
 
 const form = document.querySelector('form') as HTMLFormElement;
-const qrFileInput = form.querySelector(
-  'input#qr-file-input'
-) as HTMLInputElement;
-const qrFileLabel = form.querySelector(
-  'div#qr-file-label'
-) as HTMLLabelElement;
-const MigrationURIInput = form.querySelector(
-  'input#migration-uri'
-) as HTMLInputElement;
-const errorBox = form.querySelector('#error-box') as HTMLParagraphElement;
-const uriList = document.querySelector('#uri-list') as HTMLDivElement;
-const uriListHeader = document.querySelector(
-  '#uri-list-header'
-) as HTMLHeadingElement;
-const uriDownloadLink = document.querySelector(
-  '#uris-download-link'
-) as HTMLAnchorElement;
+const qrFileInput = form.querySelector<HTMLInputElement>('input#qr-file-input');
+const qrFileLabel = form.querySelector<HTMLDivElement>('div#qr-file-label');
+const migrationURIInput = form.querySelector<HTMLInputElement>('input#migration-uri') ;
+const errorBox = form.querySelector<HTMLParagraphElement>('#error-box');
+const uriList = document.querySelector<HTMLDivElement>('#uri-list') ;
+const uriListHeader = document.querySelector<HTMLHeadingElement>('#uri-list-header');
+const uriDownloadLink = document.querySelector<HTMLAnchorElement>('#uris-download-link');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  processMigrationURI(MigrationURIInput.value);
+
+  if(!migrationURIInput) { return; }
+  processMigrationURI(migrationURIInput.value);
 });
 
-MigrationURIInput.addEventListener('focusout', () => {
-  processMigrationURI(MigrationURIInput.value);
-});
+if(migrationURIInput) {
+  migrationURIInput.addEventListener('focusout', () => {
+    processMigrationURI(migrationURIInput.value);
+  });
+}
 
-qrFileInput.addEventListener('change', async (e: Event) => {
-  try {
-    errorBox.innerText = '';
-
-    const input = e.target as HTMLInputElement;
-
-    if(!input || !input.files?.length) {
-      throw new Error("Couldn't read QR code");
-    }
-
-    const file = input.files[0];
-    qrFileLabel.innerText = file.name;
-    const imgData = await readImageFromFile(file);
-    const data = await QrScanner.scanImage(imgData, { 
-      scanRegion: {
-        width: imgData.width, 
-        height: imgData.height 
+if(qrFileInput) {
+  qrFileInput.addEventListener('change', async (e: Event) => {
+    try {
+      if(errorBox) { errorBox.innerText = ''; }
+  
+      const input = e.target as HTMLInputElement;
+  
+      if(!input || !input.files?.length) {
+        throw new Error("Couldn't read QR code");
       }
-    });
+  
+      const file = input.files[0];
+      if(qrFileLabel) {
+        qrFileLabel.innerText = file.name;
+      }
 
-    while (uriList.firstChild) {
-      uriList.firstChild.remove();
+      const imgData = await readImageFromFile(file);
+      const data = await QrScanner.scanImage(imgData, { 
+        scanRegion: {
+          width: imgData.width, 
+          height: imgData.height 
+        }
+      });
+
+      while (uriList?.firstChild) {
+        uriList.firstChild.remove();
+      }
+
+      if(migrationURIInput) {
+        migrationURIInput.value = data.data;
+      }
+
+      processMigrationURI(data.data);
+    } catch (e) {
+      const errorString = e instanceof Error ? e.message : e;
+      console.error(errorString);
+      if(errorBox) {
+        errorBox.innerText = errorString;
+        return;
+      }
     }
-
-    MigrationURIInput.value = data.data;
-    processMigrationURI(data.data);
-  } catch (e) {
-    errorBox.innerText = e instanceof Error ? e.message : e;
-  }
-});
+  });
+}
 
 function processMigrationURI(uri: string): void {
   if(!uri) {
     return;
   }
 
-  uriListHeader.classList.add('hidden');
-  errorBox.innerText = '';
+  uriListHeader?.classList.add('hidden');
+
+  if(errorBox) { errorBox.innerText = ''; }
 
   try {
     const otpExportData = decodeMigrationUrl(uri);
@@ -79,19 +87,26 @@ function processMigrationURI(uri: string): void {
       throw new Error('No otpauth URIs in migration URI');
     }
 
-    uriListHeader.classList.remove('hidden');
-    uriDownloadLink.href = `data:text/plain;base64,${btoa(uris.join('\n'))}`;
-    uriDownloadLink.download = `gauth-export-${Date.now()}.txt`;
+    uriListHeader?.classList.remove('hidden');
 
+    if(uriDownloadLink) {
+      uriDownloadLink.href = `data:text/plain;base64,${btoa(uris.join('\n'))}`;
+      uriDownloadLink.download = `gauth-export-${Date.now()}.txt`;
+    }
+
+    if(!uriList) { return; }
+    
     for(const uri of uris) {
       const listElement = document.createElement('div');
       listElement.classList.add('list-group-item');
       const innerElement = document.createElement('code');
       innerElement.innerText = uri;
       listElement.appendChild(innerElement);
-      uriList.appendChild(listElement);
+      uriList?.appendChild(listElement);
     }
   } catch (e) {
-    errorBox.innerText = e instanceof Error ? e.message : e;
+    const errorString = e instanceof Error ? e.message : e;
+    if(errorBox) { errorBox.innerText = errorString; }
+    console.error(errorString);
   }
 }
